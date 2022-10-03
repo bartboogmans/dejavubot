9308624a843bb5d90af8cf9b7d49b2c83343fcbe
bartboogmans <bartboogmans@hotmail.com>
2022-10-03 19:08:11 +0200

    dependency update + add relevant subs/pubs

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/bart/Documents/dejavubot/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/bart/Documents/dejavubot/build'
